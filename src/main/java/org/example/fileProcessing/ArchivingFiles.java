6402f0a00ed70262ac264e25b945fc1aa800010b
Давид <108413873+Davidoose@users.noreply.github.com>
2023-01-12 23:36:49 +0300

    little changes

--- src/main/java/org/example/fileProcessing/ArchivingFiles.java
+++ src/main/java/org/example/fileProcessing/ArchivingFiles.java
@@ -3,8 +3,6 @@ package org.example.fileProcessing;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.jar.JarEntry;
-import java.util.jar.JarOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
@@ -42,17 +40,5 @@ public class ArchivingFiles {
 
             System.out.println(ex.getMessage());
         }
-    }
-
-    public static void archiveFilesJar(String fileName, String JarfileName) throws IOException {
-        JarOutputStream out = new JarOutputStream(new FileOutputStream("D:\\Industrial_Programming_Main_Project\\" + JarfileName));
-        JarEntry entry = new JarEntry(fileName);
-        out.putNextEntry(entry);
-        FileInputStream fis = new FileInputStream("D:\\Industrial_Programming_Main_Project\\" + fileName);
-        byte[] buffer = new byte[fis.available()];
-        fis.read(buffer);
-        out.write(buffer);
-        out.closeEntry();
-        out.close();
     }
 }
