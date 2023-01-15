09c8bb2e54428a78a405e03cb125a3784acffb77
Давид <108413873+Davidoose@users.noreply.github.com>
2023-01-15 14:47:21 +0300

    some tests

---
+++ src/main/java/org/example/expProcessing/ProccesExpByLib.java
@@ -0,0 +1,27 @@
+//package org.example.expProcessing;
+//
+//public class ProccesExpByLib {
+//    public static String solvingExpression(String expression) {
+//
+//import net.objecthunter.exp4j.Expression;
+//import net.objecthunter.exp4j.ExpressionBuilder;
+//
+//        public class ThirdPartyParser extends ExpressionParser {
+//            private final Expression libExp;
+//            public ThirdPartyParser(String expression)
+//            {
+//                super(expression);
+//                libExp = new ExpressionBuilder(toParse).build();
+//            }
+//            @Override
+//            public String parseExpression() {
+//                try {
+//                    double result = libExp.evaluate();
+//                    return Double.toString(result);
+//                } catch (ArithmeticException | IllegalArgumentException ex) {
+//                    return ex.getMessage();
+//                }
+//            }
+//        }
+//        return String.valueOf
+//}
